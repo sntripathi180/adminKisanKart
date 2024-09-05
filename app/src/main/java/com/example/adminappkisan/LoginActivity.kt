ab23bfb274f9e28b1fcd67731c7c03748749a3fa
ThRust2003 <rohanyadav10rbl@gmail.com>
2024-09-05 11:47:24 +0530

    this is my first commit

---
+++ app/src/main/java/com/example/adminappkisan/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.example.adminappkisan
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.adminappkisan.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private val binding:ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        binding.LoginButton.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.dontHaveAccountButton.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
